5d5099ad24671b6ebec392154f5866f041db102a
Heida Osk <heida18@ru.is>
2018-09-20 16:45:35 +0000

    set inn algorithma

---
+++ TileTraveller/TileTraveller.py
@@ -0,0 +1,5 @@
+#1 The code prints out which directions are valid.
+#2 Asks what direction player wants to travel
+#3 If direction is invalid, prints not valid
+#4 prints out valid directions every time a player moves from tile to another tile
+#5 when location of victory tile is entered, player is informed and game quits running.
